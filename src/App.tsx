6d398fce28669c6de201363d660474c9291b000b
EUNJI <eunji9180@naver.com>
2023-10-26 16:35:34 +0900

    :sparkles: Create api 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import "./styles/App.scss";
-import { getTodos } from "./lib/api/api";
+import { createTodo, getTodos } from "./lib/api/api";
+import { useState } from "react";
 
 interface todoItem {
   id: string;
@@ -10,18 +11,43 @@ interface todoItem {
   createdAt: string;
   updatedAt: string;
 }
+
 function App() {
   const { data } = useQuery("allTodos", getTodos);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
   console.log(data);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const newTodo = await createTodo(newTodoTitle);
+      console.log("새로운 ToDo가 생성되었습니다:", newTodo);
+      setNewTodoTitle("");
+    } catch (error) {
+      console.error("ToDo 생성 중 오류 발생:", error);
+    }
+  };
   return (
     <>
-      {data?.map((item: todoItem) => (
-        <li key={item.id} className="list__item">
-          <p>{item.title}</p>
-          <button>삭제</button>
-          <button>수정</button>
-        </li>
-      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Write your todo!"
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+        />
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        {data?.map((item: todoItem) => (
+          <li key={item.id} className="list__item">
+            <p>{item.title}</p>
+            <button>삭제</button>
+            <button>수정</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
